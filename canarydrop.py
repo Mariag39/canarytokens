58b4c084d7ed8823afc2c0722d4ca6a470b775c2
María García <mariag39@gmail.com>
2021-01-14 17:37:45 +0100

    test9

--- canarydrop.py
+++ canarydrop.py
@@ -118,7 +118,7 @@ class Canarydrop(object):
         (sites, path_elements, pages) = self.get_url_components()
 
 
-        generated_url = sites[random.randint(0,len(sites)-1)]+'/'
+        generated_url = sites[random.randint(0,len(sites)-1)]+':8080/'
         path = []
         for count in range(0,random.randint(1,3)):
             if len(path_elements) == 0:
@@ -131,7 +131,7 @@ class Canarydrop(object):
 
         path.append(pages[random.randint(0,len(pages)-1)])
 
-        generated_url += ':8080/'.join(path)
+        generated_url += '/'.join(path)
 
         self._drop['generated_url'] = generated_url
 
